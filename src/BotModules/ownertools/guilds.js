6738ce14c7402816e4fe3cefed117c472685bb91
Tatu Toikkanen <tatatofly@gmail.com>
2018-04-07 16:44:06 +0300

    Updated security

--- src/BotModules/ownertools/guilds.js
+++ src/BotModules/ownertools/guilds.js
@@ -16,21 +16,25 @@ class GuildsCommand extends Commando.Command {
 
     async run(message, args) {
         try {
-            var apiUrl = config.discordBotsApiUrl;
-            var guildsSize = this.client.guilds.size;
-            var apiKey = config.discordBotsApiKey;
-            var apiPost = {"server_count" : guildsSize};
-            message.channel.send("I'm in " + guildsSize + " guilds :hushed:");
-            var headers = {
-                'Content-Type': 'application/json',
-                'Authorization': apiKey
-            };
-        
-            request.post({ url: apiUrl, json: apiPost, headers: headers}, function(err, httpResponse, body) {
-                    if (err) {
-                        return console.error('post failed:', err);
-                }
-            });
+            var messageUserID = message.author.id;
+            messageUserID = messageUserID.toString();
+            if(config.botOwnerID === messageUserID){
+                var apiUrl = config.discordBotsApiUrl;
+                var guildsSize = this.client.guilds.size;
+                var apiKey = config.discordBotsApiKey;
+                var apiPost = {"server_count" : guildsSize};
+                message.channel.send("I'm in " + guildsSize + " guilds :hushed:");
+                var headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': apiKey
+                };
+            
+                request.post({ url: apiUrl, json: apiPost, headers: headers}, function(err, httpResponse, body) {
+                        if (err) {
+                            return console.error('post failed:', err);
+                    }
+                });
+            }
         } catch(e) {
             console.log(e);
         }   
