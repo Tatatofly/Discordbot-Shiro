20bfecc6e53d305c33bfe84c0e5585e67e145409
Tatu Toikkanen <git@tatu.moe>
2020-09-11 09:34:19 +0300

    new command: Rule34 (#17)

---
+++ commands/rule34.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch')
+const parseString = require('xml2js').parseString;
+
+async function getLewd(message, args) {
+  const arguments = args[0] ? args[0] : ''
+  const url = 'https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + arguments;
+  try {
+    const response = await fetch(url)
+    apiData = await response.text()
+    parseString(apiData, function (error, result) {
+      let postCount = result.posts.$.count - 1;
+      if(postCount > 100) {
+        postCount = 100;
+      }
+      if(postCount > 0) {
+        var picNum = Math.floor(Math.random() * postCount) + 0;
+        var r34Pic = result.posts.post[picNum].$.file_url;
+        message.channel.send({
+          files: [r34Pic]
+        });
+      } else {
+        message.channel.send("Nobody here but us chickens!");
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    message.channel.send("There was error with rule34.xxx")
+  }
+}
+
+module.exports = {
+	name: 'rule34',
+  cooldown: 30,
+  guildOnly: false,
+  ownerOnly: false,
+  nsfw: true,
+	description: 'Posts lewd image from rule34.xxx :sweat_drops:',
+	execute(message, args) {
+		getLewd(message, args)
+	}
+}
