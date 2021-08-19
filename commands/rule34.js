f5d3a747b2b9668bcc0d46886972c1a84835602c
Tatu <git@tatu.moe>
2021-08-19 10:09:31 +0300

    Fixed Bug #21 rule34 doesn't return anything if random file is too large

--- commands/rule34.js
+++ commands/rule34.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
-const parseString = require('xml2js').parseString;
+const xml2js = require('xml2js');
 
 async function getLewd(message, args) {
   const arguments = args[0] ? args[0] : ''
@@ -7,28 +7,34 @@ async function getLewd(message, args) {
   try {
     const response = await fetch(url)
     apiData = await response.text()
-    parseString(apiData, function (error, result) {
-      let postCount = result.posts.$.count - 1;
+
+    var parser = new xml2js.Parser();
+    parser.parseStringPromise(apiData).then(function (r34Result) {
+      let postCount = r34Result.posts.$.count - 1;
       if(postCount > 100) {
         postCount = 100;
       }
       if(postCount > 0) {
         var picNum = Math.floor(Math.random() * postCount) + 0;
-        var r34Pic = result.posts.post[picNum].$.file_url;
-        var r34Preview = result.posts.post[picNum].$.sample_url;
-        try {
-          message.channel.send({
-            files: [r34Pic]
-          });
-        } catch (error) {
-          message.channel.send({
-            files: [r34Preview]
-          });
-        }
+        var r34Pic = r34Result.posts.post[picNum].$.file_url;
+        fetch(r34Pic, {method: 'HEAD'}).then(function (r34PicResponse){
+          r34PicSize = r34PicResponse.headers.get("Content-Length");
+          if(parseInt(r34PicSize, 10) < 8000000) {
+            message.channel.send({
+              files: [r34Pic]
+            });
+          } else {
+            message.channel.send("File was too large ;__;");
+          }
+        })
       } else {
         message.channel.send("Nobody here but us chickens!");
       }
     })
+    .catch(function (error) {
+      console.log(error)
+      message.channel.send("There was error with rule34 content")
+    });
   } catch (error) {
     console.log(error)
     message.channel.send("There was error with rule34.xxx")
