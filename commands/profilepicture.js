124899a5b1ec8e7f5b37b2136751ca81f7622494
Tatu <git@tatu.moe>
2021-08-20 11:25:00 +0300

    New command ProfilePicture.js

---
+++ commands/profilepicture.js
@@ -0,0 +1,25 @@
+const config = require("../config.json");
+
+async function setPicture(message, args, user) {
+  if (!args.length) return user.setAvatar(config.profilePicURL);
+  try {
+    user.setAvatar(args[0]);
+    console.log(`New profile pic: ${args[0]}`);
+  } catch (error) {
+    console.log(error)
+    message.channel.send(`There was an error while setting profile picture: ${error.message}`)
+  }
+}
+
+module.exports = {
+  name: 'profilepicture',
+  aliases: ['profpic'],
+  cooldown: 5,
+  guildOnly: false,
+  ownerOnly: true,
+  nsfw: false,
+	description: 'Sets bot profile picture',
+	execute(message, args, client) {
+		setPicture(message, args, client.user)
+	}
+}
