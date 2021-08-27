812170c25cbc06bc07423f76677ec4850c63c885
Tatu <git@tatu.moe>
2021-08-27 11:16:42 +0300

    fixed empty suggestion issue

--- commands/suggest.js
+++ commands/suggest.js
@@ -8,6 +8,7 @@ module.exports = {
   nsfw: false,
 	description: 'Saves suggested feature for bot owner',
 	execute(message, args, client) {
+    if (!args.length || args.length < 1) return message.channel.send("You didn't suggest anything...")
     try {
       client.channels.fetch(suggestChannelID)
         .then(channel => channel.send(args.join(" ")));
